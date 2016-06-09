389913f3a8e65315a146b6c6131964df6e1318c6
Christopher Vogt <oss.nsp@cvogt.org>
2016-06-09 16:06:35 -0400

    add developer handles as vals

--- XdotaiFreeSoftwareBuild.scala
+++ XdotaiFreeSoftwareBuild.scala
@@ -1,10 +1,14 @@
+package ai.x.build
 import cbt._
 import java.net.URL
 import scala.collection.immutable.Seq
-trait XdotaiOssBuild extends PublishBuild with mixins.Suggested with mixins.Github{
+trait XdotaiFreeSoftwareBuild extends PublishBuild with mixins.Suggested with mixins.Github{
   def user = "xdotai"
   def groupId = "ai.x"
   def organizationName = "x.ai"
   def organizationUrl = new URL("http://x.ai")
   def licenses = Seq(License.Apache2)
+}
+object team{
+  val cvogt = Developer("cvogt", "Jan Christopher Vogt", "-5", new URL("https://github.com/cvogt/"))
 }
