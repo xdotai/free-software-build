31287947503f547e31f6c166a2fa87f5b7283d91
Christopher Vogt <oss.nsp@cvogt.org>
2016-06-18 23:05:02 -0400

    upgrade cbt

--- XdotaiFreeSoftwareBuild.scala
+++ XdotaiFreeSoftwareBuild.scala
@@ -1,12 +1,10 @@
 package ai.x.build
 import cbt._
 import java.net.URL
-import scala.collection.immutable.Seq
-trait XdotaiFreeSoftwareBuild extends PublishBuild with mixins.Suggested with mixins.Github{
+trait XdotaiFreeSoftwareBuild extends Publish with AdvancedScala with GithubPom{
   def user = "xdotai"
   def groupId = "ai.x"
-  def organizationName = "x.ai"
-  def organizationUrl = new URL("http://x.ai")
+  def organization = Some( Organization( "x.ai", Some( new URL("http://x.ai") ) ) )
   def licenses = Seq(License.Apache2)
 }
 object team{
