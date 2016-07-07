58794ba2e3f0a478dbc4c85e2bd92d4802d08b48
Christopher Vogt <oss.nsp@cvogt.org>
2016-07-06 21:37:51 -0400

    upgrade

--- build/build.scala
+++ build/build.scala
@@ -1,3 +1,3 @@
 import cbt._
-// cbt:https://github.com/cvogt/cbt.git#bc2231720d3620b5e0459fa12c467bf675fcfdf5
+// cbt:https://github.com/cvogt/cbt.git#b5d86995128a45c33117ecfb7365f0eb2b450a61
 class Build(val context: Context) extends Plugin
