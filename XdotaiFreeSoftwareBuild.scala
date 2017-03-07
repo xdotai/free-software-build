4b55fe6dd8e1f0d5aa7977bda70295232a41d7c0
Christopher Vogt <oss.nsp@cvogt.org>
2017-03-06 20:41:05 -0500

    upgrade cbt and add a few features

--- XdotaiFreeSoftwareBuild.scala
+++ XdotaiFreeSoftwareBuild.scala
@@ -1,11 +1,11 @@
 package ai.x.build
 import cbt._
 import java.net.URL
-trait XdotaiFreeSoftwareBuild extends Publish with AdvancedScala with GithubPom{
+trait XdotaiFreeSoftwareBuild extends Publish with AdvancedScala with GithubPom with DynamicOverrides with SnapshotVersion{
   def user = "xdotai"
   def groupId = "ai.x"
   def organization = Some( Organization( "x.ai", Some( new URL("http://x.ai") ) ) )
-  def licenses = Seq(License.Apache2)
+  def licenses = Seq( License.Apache2 )
 }
 object team{
   val cvogt = Developer("cvogt", "Jan Christopher Vogt", "-5", new URL("https://github.com/cvogt/"))
